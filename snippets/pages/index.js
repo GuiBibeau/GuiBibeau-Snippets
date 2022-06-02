3615956da8e86d03bf6a6f634e2a4f8eabc8f787
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-06-02 17:05:50 -0400

    update link

--- snippets/pages/index.js
+++ snippets/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function () {
   return (
     <ul>
-      <Link href="/suspend-swr">Suspend with SWR</Link>
+      <Link href="/mexican-suspense">Suspend with SWR</Link>
     </ul>
   );
 }
